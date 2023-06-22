56f4648264e191844e76bae9b63fd52e4592fc38
Dilara Özkoc <dilaraozkoc26@gmail.com>
2023-06-22 22:17:37 +0300

    Revert "to add new branch"
    This reverts commit 07b9e3ae4863e5e0d5e693da27d5756b70dc9ef9.

--- App.Web.Mvc/App.Web.Mvc/wwwroot/P015Etut07-master/P015Etut07/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace P015Etut07.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- App.Web.Mvc/App.Web.Mvc/wwwroot/P015Etut07-master/P015Etut07/Models/PostViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace P015Etut07.Models
-{
-    public class PostViewModel
-    {
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
-    }
-}
