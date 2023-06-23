76c3509f30a7623620e4f3a0d8242530e16fbae3
Dilara Özkoc <dilaraozkoc26@gmail.com>
2023-06-23 23:43:22 +0300

    Görevv 7  bitti

---
+++ App.Web.Mvc/App.Web.Mvc/ViewComponents/ProductViewViewComponent.cs
@@ -0,0 +1,21 @@
+﻿namespace App.Web.Mvc.ViewComponents;
+using App.Web.Mvc.Models;
+using Microsoft.AspNetCore.Mvc;
+
+public class ProductViewViewComponent : ViewComponent
+{
+    public async Task<IViewComponentResult> InvokeAsync(string title, int starCount, decimal oldPrice, decimal price, string imageUrl, bool isScale)
+    {
+        var product = new Product()
+        {
+            Title = title,
+            StarCount = starCount,
+            OldPrice = oldPrice,
+            Price = price,
+            ImageUrl = imageUrl,
+            isSale = isScale
+        };
+        return View();
+    }
+}
+
